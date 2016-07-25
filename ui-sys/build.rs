e2e36d3f1267d24bcf334573a155ab0b03b1129c
Bill Fraser <wfraser@codewise.org>
2016-07-25 13:54:51 -0700

    update to latest libui

--- ui-sys/build.rs
+++ ui-sys/build.rs
@@ -1,6 +1,6 @@
+extern crate cmake;
 extern crate make_cmd;
 
-use std::env;
 use std::path::Path;
 use std::process::Command;
 
@@ -9,14 +9,11 @@ fn main() {
         Command::new("git").args(&["submodule", "update", "--init"]).status().unwrap();
     }
 
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let outdir_argument = format!("OUTDIR={}", out_dir);
-    let objdir_argument = format!("OBJDIR={}/obj", out_dir);
-    make_cmd::gnu_make().args(&["-C", "libui", &*outdir_argument, &*objdir_argument])
-                        .status()
-                        .unwrap();
+    let dst = cmake::Config::new("libui")
+                            .build_target("")
+                            .build();
+    let out_dir = dst.display();
 
     println!("cargo:rustc-link-lib=dylib=ui");
     println!("cargo:rustc-link-search=native={}", out_dir);
 }
-
